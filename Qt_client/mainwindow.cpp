fefeafd63490a3b5120131be157e884dd723f28f
elekmad <damien.warin@gmail.com>
2016-10-18 17:06:44 +0200

    * Tags splittés en include et exclude dans l'interface * Début de la fenetre de demande de nouveau nom de tags.

--- Qt_client/mainwindow.cpp
+++ Qt_client/mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "GetTagName.h"
 #include <qwidget.h>
 #include <qlayout.h>
 #include <qlogging.h>
@@ -40,7 +41,7 @@ void MainWindow::reload_file_list(void)
         QStringListModel *m = new QStringListModel();
         while(ptr != NULL)
         {
-            list << ptr->name;
+            list << File_get_name(ptr);
             ptr = File_get_next(ptr);
         }
         m->setStringList(list);
@@ -59,24 +60,49 @@ void MainWindow::reload_tags_list(void)
     ltags = TagFolder_list_tags(&folder);
     if(ltags != NULL)
     {
-        QVBoxLayout *TagsListLayout = new QVBoxLayout();
-        TagsListLayout->setSizeConstraint(TagsListLayout->SetMinimumSize);
-        QWidget *TagsList = this->findChild<QWidget*>("TagsList");
+        QVBoxLayout *IncludeTagsListLayout = new QVBoxLayout();
+        IncludeTagsListLayout->setSizeConstraint(IncludeTagsListLayout->SetMinimumSize);
+        QWidget *IncludeTagsList = this->findChild<QWidget*>("IncludeTagsList");
+        QVBoxLayout *ExcludeTagsListLayout = new QVBoxLayout();
+        ExcludeTagsListLayout->setSizeConstraint(ExcludeTagsListLayout->SetMinimumSize);
+        QWidget *ExcludeTagsList = this->findChild<QWidget*>("ExcludeTagsList");
+
+        connect(IncludeTagsList, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(on_IncludeTag_customContextMenuRequested(QPoint)));
+        connect(ExcludeTagsList, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(on_ExcludeTag_customContextMenuRequested(QPoint)));
+
         Tag *ptr = ltags;
 
         while(ptr != NULL)
         {
             QCheckBox *checkBox;
-            QString checkbox_name(ptr->name);
+            QString checkbox_name(Tag_get_name(ptr));
             checkBox = new QCheckBox;
             checkBox->setObjectName(checkbox_name);
             checkBox->setText(checkbox_name);
-            TagsListLayout->addWidget(checkBox);
+            checkBox->setContextMenuPolicy(Qt::CustomContextMenu);
             connect(checkBox, SIGNAL(clicked(bool)), this, SLOT(on_checkBox_clicked(bool)));
+
+            switch(Tag_get_type(ptr))
+            {
+                case TagTypeInclude :
+                    IncludeTagsListLayout->addWidget(checkBox);
+                    connect(checkBox, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(on_IncludeTag_customContextMenuRequested(QPoint)));
+                    break;
+                case TagTypeExclude :
+                    ExcludeTagsListLayout->addWidget(checkBox);
+                    connect(checkBox, SIGNAL(customContextMenuRequested(QPoint)), this, SLOT(on_ExcludeTag_customContextMenuRequested(QPoint)));
+                    break;
+            }
 
             ptr = Tag_get_next(ptr);
         }
-        TagsList->setLayout(TagsListLayout);
+        IncludeTagsList->setLayout(IncludeTagsListLayout);
+        if(IncludeTagsListLayout->count() == 0)
+            IncludeTagsList->setVisible(false);
+        ExcludeTagsList->setLayout(ExcludeTagsListLayout);
+        if(ExcludeTagsListLayout->count() == 0)
+            ExcludeTagsList->setVisible(false);
+        qInfo() << "exclude : " << ExcludeTagsListLayout->count() << "include : " << IncludeTagsListLayout->count();
 
         ui->retranslateUi(this);
         QMetaObject::connectSlotsByName(this);
@@ -95,6 +121,41 @@ void MainWindow::on_checkBox_clicked(bool checked)
     reload_file_list();
 }
 
+void MainWindow::on_IncludeTag_customContextMenuRequested(const QPoint &pos)
+{
+    on_Tag_customContextMenuRequested(true);
+    tag_operation->tag_type = TagTypeInclude;
+}
+
+void MainWindow::on_ExcludeTag_customContextMenuRequested(const QPoint &pos)
+{
+    on_Tag_customContextMenuRequested(false);
+    tag_operation->tag_type = TagTypeExclude;
+}
+
+void MainWindow::on_Tag_customContextMenuRequested(bool including)
+{
+    QWidget *w_sender = qobject_cast<QWidget*>(sender());
+    QCheckBox *checkBox;
+    QMenu *menu = new QMenu(w_sender);
+    QAction *action;
+    if(including)
+        action = menu->addAction(tr("Ajouter un Tag inclusif"));
+    else
+        action = menu->addAction(tr("Ajouter un Tag exclusif"));
+    checkBox = qobject_cast<QCheckBox*>(w_sender);
+    if(checkBox != NULL)
+    {
+        qInfo() << checkBox->text();
+        action = menu->addAction(tr("Renommer \"") + checkBox->text() + tr("\""));
+        action = menu->addAction(tr("Supprimer \"") + checkBox->text() + tr("\""));
+    }
+    menu->exec(QCursor::pos());
+
+    //GetTagName Dialog(this);
+    //Dialog.exec();
+}
+
 void MainWindow::on_FileList_customContextMenuRequested(const QPoint &pos)
 {
     QListView *filelist = this->findChild<QListView*>("FileList");
@@ -104,25 +165,25 @@ void MainWindow::on_FileList_customContextMenuRequested(const QPoint &pos)
     QMap<QString, Tag*> File_Tags;
     Tag *all_tags = TagFolder_list_tags(&folder), *file_tags = TagFolder_get_tags_tagging_specific_file(&folder, file_selected.toLocal8Bit().data()), *ptr;
 
-    operation = new FileOperation;
-    operation->file_name = file_selected;
+    file_operation = new FileOperation;
+    file_operation->file_name = file_selected;
     ptr = file_tags;
     while(ptr != NULL)
     {
-        File_Tags[tr(ptr->name)] = ptr;
+        File_Tags[tr(Tag_get_name(ptr))] = ptr;
         ptr = Tag_get_next(ptr);
     }
     ptr = all_tags;
     while(ptr != NULL)
     {
-        action = menu->addAction(tr(ptr->name));
+        action = menu->addAction(tr(Tag_get_name(ptr)));
         action->setCheckable(true);
         connect(action, SIGNAL(toggled(bool)), this, SLOT(do_operation_on_file_window(bool)));
-        if(File_Tags[tr(ptr->name)] != NULL)
+        if(File_Tags[tr(Tag_get_name(ptr))] != NULL)
             action->setChecked(true);
         ptr = Tag_get_next(ptr);
     }
-    menu->exec(pos + filelist->pos() + this->pos());
+    menu->exec(QCursor::pos());
 }
 
 void MainWindow::do_operation_on_file_window(bool add_or_del)
@@ -135,21 +196,36 @@ void MainWindow::do_operation_on_file_window(bool add_or_del)
     else
         qInfo() << "Retirer un tag de : " << file_selected;
 
-    operation->add_or_del = add_or_del;
-    operation->tag_name = qobject_cast<QAction*>(sender())->text();
+    file_operation->add_or_del = add_or_del;
+    file_operation->tag_name = qobject_cast<QAction*>(sender())->text();
     do_operation_on_file();
 }
 
 void MainWindow::do_operation_on_file()
 {
-    if(operation->add_or_del == true)
+    if(file_operation->add_or_del == true)
     {
-        TagFolder_tag_a_file(&folder, operation->file_name.toLocal8Bit().data(), operation->tag_name.toLocal8Bit().data());
+        TagFolder_tag_a_file(&folder, file_operation->file_name.toLocal8Bit().data(), file_operation->tag_name.toLocal8Bit().data());
         qInfo() << "Add tag to file operation";
     }
     else
     {
-        TagFolder_untag_a_file(&folder, operation->file_name.toLocal8Bit().data(), operation->tag_name.toLocal8Bit().data());
+        TagFolder_untag_a_file(&folder, file_operation->file_name.toLocal8Bit().data(), file_operation->tag_name.toLocal8Bit().data());
         qInfo() << "Del tag from file operation";
+    }
+}
+
+void MainWindow::do_operation_on_tag()
+{
+    switch(tag_operation->op_type)
+    {
+        case OpTypeDel :
+            TagFolder_delete_tag(&folder, tag_operation->tag_name.toLocal8Bit().data());
+            qInfo() << "Del a tag operation";
+            break;
+        case OpTypeAdd :
+            TagFolder_create_tag(&folder, tag_operation->tag_name.toLocal8Bit().data(), tag_operation->tag_type);
+            qInfo() << "Create a tag operation";
+            break;
     }
 }
--- Qt_client/mainwindow.h
+++ Qt_client/mainwindow.h
@@ -8,6 +8,13 @@ extern "C"
 #include "/home/damien/dev/TagFolders/TagFolder.h"
 }
 
+enum OperationType
+{
+    OpTypeDel,
+    OpTypeAdd,
+};
+typedef enum OperationType OperationType;
+
 namespace Ui {
 class MainWindow;
 }
@@ -18,6 +25,14 @@ public :
     QString tag_name;
     QString file_name;
     bool add_or_del;
+};
+
+class TagOperation
+{
+public :
+    QString tag_name;
+    OperationType op_type;
+    TagType tag_type;
 };
 
 class MainWindow : public QMainWindow
@@ -33,17 +48,22 @@ public:
 private slots:
 
     void on_FileList_customContextMenuRequested(const QPoint &pos);
+    void on_IncludeTag_customContextMenuRequested(const QPoint &pos);
+    void on_ExcludeTag_customContextMenuRequested(const QPoint &pos);
+    void on_Tag_customContextMenuRequested(bool including);
     void do_operation_on_file_window(bool);
 
 public slots:
     void on_checkBox_clicked(bool checked);
     void do_operation_on_file();
+    void do_operation_on_tag();
 
 private:
     Ui::MainWindow *ui;
     QStringListModel *file_model;
     TagFolder folder;
-    FileOperation *operation;
+    FileOperation *file_operation;
+    TagOperation *tag_operation;
 };
 
 #endif // MAINWINDOW_H
